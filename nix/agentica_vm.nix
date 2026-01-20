f6227a29d648190d06b0e7a85bc46c68ffe08a37
MathisWellmann <wellmannmathis@gmail.com>
2026-01-20 15:11:05 +0000

    Add agentica vm which can be run with `nix-build nix/agentica_vm.nix; result/bin/run-agentica-vm`

---
+++ nix/agentica_vm.nix
@@ -0,0 +1,50 @@
+# Build and run with `nix-build nix/agentica_vm.nix; result/bin/run-agentica-vm`
+# The jupyter notebook will be available on `127.0.0.1:8888` on the host.
+let
+  pkgs = import <nixpkgs> {};
+
+  agentica_vm = {...}: let
+    port = 8888;
+  in {
+    boot = {
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+      initrd.systemd.enable = true;
+    };
+
+    services = {
+      getty.autologinUser = "root";
+      jupyter = {
+        enable = true;
+        inherit port;
+        ip = "0.0.0.0";
+        # Disable both token and password.
+        password = "";
+        command = "jupyter notebook --NotebookApp.token='' --NotebookApp.password='' ";
+        # extraPackages = pkgs.python3.pkgs; # Just give it all packages.
+      };
+    };
+    networking = {
+      hostName = "agentica";
+      firewall.allowedTCPPorts = [port];
+      interfaces.eth0.ipv4.addresses = [
+        {
+          address = "192.168.0.124";
+          prefixLength = 24;
+        }
+      ];
+    };
+    virtualisation = {
+      diskSize = 2048;
+      # This enables port forwarding
+      vmVariant.virtualisation.qemu.options = [
+        "-netdev" "user,id=net0,hostfwd=tcp::${toString port}-:${toString port}"
+        "-device" "virtio-net-pci,netdev=net0"
+      ];
+    };
+  };
+  vms = pkgs.nixos [
+    agentica_vm
+  ];
+in
+  vms.config.system.build.vm
