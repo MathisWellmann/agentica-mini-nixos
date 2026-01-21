5fd08d08a720aaadb4fc886d1bf684ed64ee302d
MathisWellmann <wellmannmathis@gmail.com>
2026-01-21 16:22:56 +0000

    nix: more work on agentica VM

--- nix/agentica_vm.nix
+++ nix/agentica_vm.nix
@@ -10,6 +10,7 @@ let
       loader.systemd-boot.enable = true;
       loader.efi.canTouchEfiVariables = true;
       initrd.systemd.enable = true;
+      supportedFilesystems = [ "9p" "9p2000" ];
     };
 
     services = {
@@ -21,7 +22,6 @@ let
         # Disable both token and password.
         password = "";
         command = "jupyter notebook --NotebookApp.token='' --NotebookApp.password='' ";
-        # extraPackages = pkgs.python3.pkgs; # Just give it all packages.
       };
     };
     networking = {
@@ -34,12 +34,26 @@ let
         }
       ];
     };
+    environment.systemPackages = with pkgs; [
+      uv
+      neofetch
+    ];
+    # Mount the host directory `vm_persistent_state` into the VM  at `/mnt/vm_persistent_state` for persistent agent storage.
+    fileSystems."/mnt/shared" = {
+      device = "hostshare"; # must match the 'security.model' name in qemu
+      fsType = "9p";
+      options = [
+        "trans=virtio"
+        "version=9p2000.L"
+      ];
+    };
     virtualisation = {
       diskSize = 2048;
       # This enables port forwarding
       vmVariant.virtualisation.qemu.options = [
         "-netdev" "user,id=net0,hostfwd=tcp::${toString port}-:${toString port}"
         "-device" "virtio-net-pci,netdev=net0"
+        "-virtfs local,path=${toString ./../hostshare},mount_tag=hostshare,security_model=mapped-xattr,id=hostshare"
       ];
     };
   };
