367dc716e144e0d8fbb9417b9397d9c4968c0da5
MathisWellmann <wellmannmathis@gmail.com>
2026-01-21 16:33:48 +0000

    Cleanup `agentica-mini`

--- agentica/__init__.py
+++
@@ -1,4 +0,0 @@
-from agentica.agent import Agent, AgentError
-from agentica.runtime import AgentRuntime, local_runtime, spawn
-
-__all__ = ["local_runtime", "spawn", "AgentRuntime", "Agent", "AgentError"]
